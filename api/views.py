06cf11e6ec8bc7c49e9ec63f3d5a932a63d88898
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2024-10-26 22:45:31 +0300

    Add file uploading

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
+import os
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
+from django.core.files.storage import default_storage
 from drf_yasg.utils import swagger_auto_schema
 
 from .utils import make_predictions
@@ -28,7 +30,12 @@ class CSVProcessViewSet(GenericViewSet):
         clients = serializer.validated_data['clients']
         transactions = serializer.validated_data['transactions']
 
-        result_csv = make_predictions(clients, transactions, 'result.csv')
+        path_clients = os.path.join('uploads/', clients.name)
+        full_path_clients = default_storage.save(path_clients, clients)
+        path_transactions = os.path.join('uploads/', transactions.name)
+        full_path_transactions = default_storage.save(path_transactions, transactions)
+
+        result_csv = make_predictions(full_path_clients, full_path_transactions, 'result.csv')
 
         response = Response(result_csv, content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="result.csv"'
